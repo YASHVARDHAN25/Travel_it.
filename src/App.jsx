22e540ce15956e5f61ec4527adba350438045991
yashvardhan <yashuitjy112@gmail.com>
2024-04-24 01:46:30 +0530

    respon

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
 const locomotiveScroll = new LocomotiveScroll();
   const [count, setCount] = useState(0)
   return (
-    <div className='text-white overflow-x-hidden'>
+    <div className='text-white w-screen overflow-x-hidden'>
       <Navbar />
       <Landing />
       <Menu />
